4dbce80697740ebdcad012735d3ab0e05d99ecd2
Matthew Downey <mgd020@gmail.com>
2021-09-28 21:06:31 +1000

    Reserve pypi package name

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="mustachio",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://github.com/mgd020/mustacheo",
+    author="https://github.com/mgd020",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
